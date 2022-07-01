c7189378966ac2ce219291f121dc6bcd77f66512
Redouane <61066248+brokenyouth@users.noreply.github.com>
2022-07-01 10:11:23 +0200

    cmake base files

---
+++ asterisk/asterisk.cpp
@@ -0,0 +1,12 @@
+﻿// asterisk.cpp : définit le point d'entrée de l'application.
+//
+
+#include "asterisk.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
